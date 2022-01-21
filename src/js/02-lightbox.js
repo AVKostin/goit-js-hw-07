7c409e394908e8c0e3c37851c2c02d0fbfe94635
AVKostin <alexander.kostin.ua@gmail.com>
2022-01-21 15:23:02 +0200

    ready Home Task
    ready Home Task

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const itemGalleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML("beforeend", itemGalleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+			<a class="gallery__link" href="${original}">
+				<img
+				class="gallery__image"
+					src="${preview}"
+					alt="${description}"
+				/>
+			</a>`;
+    })
+    .join("");
+}
+let lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
