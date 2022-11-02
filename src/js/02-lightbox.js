92b019a4951fd64690416835ea3b079407343f60
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-11-02 17:17:44 +0200

    refactoring

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -6,20 +6,20 @@ const itemGalleryMarkup = createGalleryMarkup(galleryItems);
 galleryRef.insertAdjacentHTML("beforeend", itemGalleryMarkup);
 
 function createGalleryMarkup(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
-			<a class="gallery__link" href="${original}">
-				<img
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+                <a class="gallery__link" href="${original}">
+                  <img
                     class="gallery__image"
-                        src="${preview}"
-                        alt="${description}"
-				/>
-			</a>`;
-    })
-    .join("");
+                    src="${preview}"
+                    alt="${description}"
+                  />
+                </a>`;
+        })
+        .join("");
 }
 new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
-  captionDelay: 250,
+    captionsData: "alt",
+    captionDelay: 250,
 });
